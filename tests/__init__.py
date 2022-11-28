fe28137b7c8aa508bcc76bac9246990871aa4a51
CoderWanFeng <1957875073@qq.com>
2022-11-28 21:32:09 +0800

    add 18套模板：https://www.bilibili.com/video/BV1Kz4y1r76w

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,5 @@
-# pip install python-office -i https://pypi.python.org/simple -U
-# pip install PyOfficeRobot -i https://pypi.python.org/simple -U
-# pip install poimage -i https://pypi.python.org/simple -U
+# pip install pydatav -i https://pypi.python.org/simple -U
 
 # 1、pip freeze > allpackages.txt
 # 2、pip uninstall -r allpackages.txt -y
-# 3、pip install --upgrade python-office
+# 3、pip install --upgrade python-pydatav
--- tests/test_unit/test_ppt.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-from office.api.ppt import *
-
-
-class TestPPT(unittest.TestCase):
-    def test_ppt2pdf(self):
-        ppt2pdf(path=r'./test_files/ppt/')
